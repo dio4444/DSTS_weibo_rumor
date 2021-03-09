2e13c913da54389fa7cf450b86e953afa48d785c
马智华 <51101061+dio4444@users.noreply.github.com>
2021-03-09 09:21:35 +0800

    Update LDA_use.py
    LDA主题输入改成了分词结果，大大加快了训练时间

--- LDA/LDA_use.py
+++ LDA/LDA_use.py
@@ -17,12 +17,7 @@ for line in open('F:/代码code/DSTS/dict/Stopword.txt', encoding='UTF-8'):
 
 def LDA_topic(text):
     vector = []
-    seg_list = jieba.lcut(text, cut_all=False)  # 使用分词，将文本分开 生成列表
-    result = []
-    for j in seg_list:  # 去掉停用词
-        if j not in stopwords and j is not ' ':
-            result.append(j)
-    result = [result]
+    result = [text]
     dictionary = corpora.Dictionary(result)  # 构造词典，给每一个词创建一个索引号
     # 使用上面的词典，将转换文档列表（语料）变成 DT 矩阵
     doc_term_matrix = [dictionary.doc2bow(doc) for doc in result]
@@ -34,10 +29,16 @@ def LDA_topic(text):
 
 
 if __name__ == '__main__':
-    with open("../rumor/4473713031.json", 'r', encoding='UTF-8')as f:
+    with open("../rumor/4016873519.json", 'r', encoding='UTF-8')as f:
         weibo_centent = f.read()
         weibo_dict = json.loads(weibo_centent)  # 将 JSON 对象转换为 Python 字典
         for i in range(0, len(weibo_dict)):
             item = weibo_dict[i]
             text1 = item['text']  # 逐条提取微博正文
-            LDA_topic(text1)
+            seg_list = jieba.lcut(text1, cut_all=False)  # 使用分词，将文本分开 生成列表
+            result = []
+            for k in seg_list:  # 去掉停用词
+                if k not in stopwords:
+                    result.append(k)
+            print(LDA_topic(result))
+
